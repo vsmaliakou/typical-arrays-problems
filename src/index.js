858068c84e3d22dc48ef0a04cb44ec835cdd0ca3
vsmaliakou <vsmaliakou@yandex.by>
2021-08-11 20:26:16 +0300

    feat: solve the task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0
+  }
+  let minValue = array[0];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] < minValue){
+      minValue = array[i];
+    }
+  }
+  return minValue;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0
+  }
+  let maxValue = array[0];
+  for(let i = 0; i < array.length; i++){
+    if(array[i] > maxValue){
+      maxValue = array[i];
+    }
+  }
+  return maxValue;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array || !array.length){
+    return 0
+  }
+  return array.reduce((a, b) => (a + b)) / array.length;
 }
